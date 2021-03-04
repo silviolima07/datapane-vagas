5d2a13271f547cf060c38811f76dc807f13d104c
silviolima07 <silvio.clima@unitau.com.br>
2021-03-04 11:12:55 -0300

    Update scrap_indeed.yml and scrap-indeed.py

--- scrap/scrap-indeed.py
+++ scrap/scrap-indeed.py
@@ -110,7 +110,7 @@ df_final = df.drop_duplicates()
 cargo = CARGO.replace(' ', '_')
 print("\nCargo:", cargo)
 filename = 'indeed_'+cargo+'.csv'
-HOME_CSV = 'CSV'
+#HOME_CSV = 'CSV'
 print("\nCriando arquivo CSV: ", filename)
-df_final.to_csv(HOME_CSV+'/'+filename, index=False, header=True)
+df_final.to_csv(filename, index=False, header=True)
 	
