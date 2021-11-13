1707925767f9caff0c95b2ae9c4f7fa236145d52
Silvio Lima <silviolima07@gmail.com>
2021-11-12 23:22:11 -0300

    Update datapane_indeed.py

--- datapane_indeed.py
+++ datapane_indeed.py
@@ -160,7 +160,8 @@ dp.Report(
                dp.Plot(fig2)
                ]
      )
-).upload(name=name=f'Vagas em Data Science', open = True, description='Vagas ---> Cientista de Dados, Analista de Dados, Engenheiro de Dados e Engenheiro de Machine Learning')
+).upload(name=f'Vagas em Data Science', open = True, description='Vagas ---> Cientista de Dados, Analista de Dados, 
+         Engenheiro de Dados e Engenheiro de Machine Learning')
 #dp.Report(pagina1, pagina2, pagina3,pagina4,pagina5).publish(name=f'Vagas em Data Science', 
 #open = True, description='Vagas ---> Cientista de Dados, Analista de Dados, Engenheiro de Dados e Engenheiro de Machine Learning').upload(name=f'Vagas em Data Science', open = True, description='Vagas ---> Cientista de Dados, Analista de Dados, Engenheiro de Dados e Engenheiro de Machine Learning')
 #
